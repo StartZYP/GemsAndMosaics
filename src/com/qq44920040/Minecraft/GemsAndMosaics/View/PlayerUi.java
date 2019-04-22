3c204113ea547eef0f7392cf0412b2e02119b1e8
Startzyp <CodeHacker@aliyun.com>
2019-04-22 11:25:21 +0800

    Merge branch 'master' of https://github.com/StartZYP/GemsAndMosaics
    # Conflicts:
    #	src/com/qq44920040/Minecraft/GemsAndMosaics/Util/publicItem.java

--- src/com/qq44920040/Minecraft/GemsAndMosaics/View/PlayerUi.java
+++ src/com/qq44920040/Minecraft/GemsAndMosaics/View/PlayerUi.java
@@ -8,30 +8,30 @@ import org.bukkit.inventory.ItemStack;
 
 public class PlayerUi {
     public static Inventory PlayerOpenMosaicGui(){
-        Inventory inventoryView = Bukkit.createInventory(null,53, ContsNumber.MosaicGuiTitle);
+        Inventory inventoryView = Bukkit.createInventory(null,54, ContsNumber.MosaicGuiTitle);
         ItemStack border = new ItemStack(Material.BREAD);
         ItemStack addblock = new ItemStack(Material.STAINED_GLASS_PANE);
         for (int i=0;i<54;i++){
             if (i<=9){
-                inventoryView.addItem(border);
+                inventoryView.setItem(i,border);
             }else if (i<=12){
-                inventoryView.addItem(addblock);
+                inventoryView.setItem(i,addblock);
             }else if (i==17||i==18){
-                inventoryView.addItem(border);
+                inventoryView.setItem(i,border);
             }else if (i==19||i==21){
-                inventoryView.addItem(addblock);
+                inventoryView.setItem(i,addblock);
             }else if (i==26||i==27){
-                inventoryView.addItem(border);
+                inventoryView.setItem(i,border);
             }else if (i>=28&&i<=30){
-                inventoryView.addItem(addblock);
+                inventoryView.setItem(i,addblock);
             }else if (i>=32&&i<=34){
-                inventoryView.addItem(addblock);
+                inventoryView.setItem(i,addblock);
             }else if (i==35||i==36){
-                inventoryView.addItem(border);
+                inventoryView.setItem(i,border);
             }else if (i>=37&&i<=43){
-                inventoryView.addItem(addblock);
+                inventoryView.setItem(i,addblock);
             }else if (i>=44){
-                inventoryView.addItem(border);
+                inventoryView.setItem(i,border);
             }
         }
         return inventoryView;
